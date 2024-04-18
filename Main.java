12d8b5f7764096b9ee37aa0287d758f557a4f37f
danishayman <danishaiman3b@gmail.com>
2024-04-19 01:45:16 +0800

    update formatting

--- Main.java
+++ Main.java
@@ -1,36 +1,103 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        String num1 = "123";
-        String num2 = "456";
-        System.out.println(multiply(num1, num2));
+        // Generate random numbers with the same number of digits
+        int numDigits = 3; // Change this to adjust the number of digits
+        // int first_number = generateRandomNumber(numDigits);
+        // int second_number = generateRandomNumber(numDigits);
+
+        int first_number = 754;
+        int second_number = 797;
+
+        int result = 0;
+
+        // get length of number assuming both number has the same length
+        int length = Math.max(String.valueOf(first_number).length(), String.valueOf(second_number).length());
+
+        // store each digit in an array
+        int[] first_number_array = new int[length];
+        int[] second_number_array = new int[length];
+        int[] carry = new int[length];
+
+        int temp_first = first_number;
+        int temp_second = second_number;
+        for (int i = 0; i < length; i++) {
+            second_number_array[i] = temp_second % 10;
+            temp_second = temp_second / 10;
+            first_number_array[i] = temp_first % 10;
+            temp_first = temp_first / 10;
+        }
+
+        // multiply each digit of the first number with the second number using simple
+        // multiplication method
+        int[] partial_result = new int[length];
+        for (int i = 0; i < length; i++) {
+            carry[i] = 0;
+
+            for (int j = 0; j < length; j++) {
+                int product = first_number_array[j] * second_number_array[i];
+
+                int last_digit = product % 10;
+                partial_result[i] = partial_result[i] + last_digit * (int) Math.pow(10, j);
+
+                // if the product is more than 9, add the tens to the carry
+                if (product > 9)
+                    carry[i] += product / 10 * (int) Math.pow(10, j + i + 1);
+            }
+            // multiply the result by 10^i for each iteration
+            partial_result[i] *= (int) Math.pow(10, i);
+        }
+
+        // print the result
+        for (int i = 0; i < length; i++) {
+            // add the result of the multiplication to the previous result
+            result += partial_result[i] + carry[i];
+        }
+
+        // print it
+        System.out.println("    " + first_number);
+        System.out.println("x   " + second_number);
+
+        String line = "";
+        // length + 7 to account for the space and the 'x' sign
+        for (int i = 0; i < length + 7; i++) {
+            line += "-";
+        }
+
+        System.out.println(line);
+        for (int i = 0; i < length; i++) {
+            String indentation = " ";
+            for (int j = length - i; j > 0; j--) {
+                indentation += " ";
+            }
+
+            String partialStr = String.format(indentation + "%0" + length + "d",
+                    partial_result[i] / (int) Math.pow(10, i));
+            String carryStr = String.format(indentation + "%" + length + "d", carry[i] / (int) Math.pow(10, i));
+
+            if (i == length - 1) {
+                carryStr = "+" + carryStr.substring(1);
+            }
+
+            System.out.printf("%-15s", partialStr);
+            System.out.println("partial products for (=" + first_number + " x " + second_number_array[i] + ")");
+            System.out.printf("%-15s", carryStr);
+            System.out.println("carry for (=" + first_number + " x " + second_number_array[i] + ")");
+
+
+        }
+        System.out.println(line);
+        System.out.println(" " + result);
+        System.out.println(line);
     }
 
-    public static String multiply(String num1, String num2) {
-        int n = num1.length();
-        int[] result = new int[2 * n];
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                int digit1 = num1.charAt(i) - '0';
-                int digit2 = num2.charAt(j) - '0';
-                int product = digit1 * digit2;
-                int p1 = i + j, p2 = i + j + 1;
-
-                int sum = product + result[p2];
-                result[p2] = sum % 10;
-                result[p1] += sum / 10;
-
-                System.out.println("Partial product: " + result[p2] + ", Carrier: " + result[p1]);
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int res : result) {
-            if (!(sb.length() == 0 && res == 0)) {
-                sb.append(res);
-            }
-        }
-
-        return sb.length() == 0 ? "0" : sb.toString();
+    // Method to generate a random number with a specified number of digits
+    public static int generateRandomNumber(int numDigits) {
+        Random rand = new Random();
+        int min = (int) Math.pow(10, numDigits - 1);
+        int max = (int) Math.pow(10, numDigits) - 1;
+        return rand.nextInt(max - min + 1) + min;
     }
 }
